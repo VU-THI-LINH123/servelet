a91754e6ea5f53aa3c9a8b196bafca3a25565db5
LinhVT15 <vuthilinh.itk12.1998@gmail.com>
2023-04-06 03:08:46 +0700

    fdjghlfdkh

--- src/main/java/servelet/contrllor/HelloWorld.java
+++ src/main/java/servelet/contrllor/HelloWorld.java
@@ -9,25 +9,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 @WebServlet("/HelloWorld")
 public class HelloWorld extends HttpServlet {
-    private String message;
- 
-    public void init() throws ServletException {
-        // Do required initialization
-        message = "Hello World";
-    }
- 
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
- 
-        // Set response content type
-        response.setContentType("text/html");
- 
-        // Actual logic goes here.
-        PrintWriter out = response.getWriter();
-        out.println("<h1>" + message + "</h1>");
-    }
- 
-    public void destroy() {
-        // do nothing.
-    }
+	 private  Integer message=new Integer(10);
+	 
+	    public void init() throws ServletException {
+	        // Do required initialization
+	        message +=8;
+	    }
+	 
+	    public void doGet(HttpServletRequest request, HttpServletResponse response)
+	            throws ServletException, IOException {
+	    	 
+	    	message +=1;
+	        // Set response content type
+	        response.setContentType("text/html");
+	 
+	        // Actual logic goes here.
+	        PrintWriter out = response.getWriter();
+	        out.println("<h1>" + message + "</h1>");
+	        message=new Integer(0);
+	    }
+	 
+	    public void destroy() {
+	        // do nothing.
+	    	message=message-2;
+	    }
 }
